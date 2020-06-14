383b645d8c84bbd5b75f64431b4c57af6b0b4786
Mikhail Matsehora <mishamacegora3@gmail.com>
2020-06-14 17:03:14 +0300

    add lesson 06

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+let userNumber,
+    systemNumber = Math.floor(Math.random() * 100);
+
+const validation = data => {
+    
+    if (data === null) {
+        alert("Конец игры");
+        return null;
+    }
+    
+    if (!isNumber(data)) {
+        validation(prompt('Введи число!'));
+    }
+
+    if (data > systemNumber) {
+        validation(prompt('Загадачное число меньше. Введите новый вариант'));
+    } else if (data < systemNumber) {
+        validation(prompt('Загадачное число больше. Введите новый вариант'));
+    } else if (data == systemNumber) {
+        return alert("Поздравляем! Вы угадали.");
+    }
+
+};
+
+userNumber = validation(prompt("Угадай число от 1 до 100"));
+
+
+
